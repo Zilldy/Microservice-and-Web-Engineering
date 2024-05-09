b3976fedcee486462570fbbff4dc8e24659943bf
Zilldy <logonrmlocal@fiap.com.br>
2024-05-08 22:01:43 -0300

    feat: Add event binding

--- aula12/src/app/app.component.ts
+++ aula12/src/app/app.component.ts
@@ -13,4 +13,25 @@ export class AppComponent {
 
   desabilitado: boolean = true;
   imageUrl: String = "https://picsum.photos/id/500/150/150";
+
+  isMouseOver: boolean = false;
+  valorAtual: string = '';
+  valorSalvo: string = '';
+
+  botaoClicado() {
+    alert('Botão clicado!');
+    this.desabilitado = !this.desabilitado;
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    this.valorAtual = event.key;
+  }
+
+  salvarValor(event: any) {
+    this.valorSalvo = event;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
 }
