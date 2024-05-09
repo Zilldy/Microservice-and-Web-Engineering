247baa6c812fc7f2da4c3c3d15151cc68ac5de8a
Zilldy <logonrmlocal@fiap.com.br>
2024-05-08 22:14:34 -0300

    feat: Add two way data binding

--- aula12/src/app/app.component.ts
+++ aula12/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -34,4 +35,6 @@ export class AppComponent {
   onMouseOverOut() {
     this.isMouseOver = !this.isMouseOver;
   }
+
+  nome : string = '';
 }
